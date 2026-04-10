cb4a1149c6112964eef1367e2b84c7654f7f53af
1mach0 <machiraju.shanmukh@gmail.com>
2026-04-10 17:21:27 +0530

    Define format and convention for cube

---
+++ config.py
@@ -0,0 +1,42 @@
+"""
+CENTRES
+U: YELLOW
+D: WHITE
+F: BLUE
+B: GREEN
+R: RED
+L: ORANGE
+
+CORNERS
+PERM
+0: (-1, -1, -1) LUF
+1: (1, -1, -1) RUF
+2 (-1, 1, -1) LDF
+3: (1, 1, -1) RDF
+4: (-1, -1, 1) LUB
+5: (1, -1, 1) RUB
+6: (-1, 1, 1) LDB
+7: (1, 1, 1) RDB
+ORIENT
+0: NORMAL
+1: CLOCKWISE
+1: ANTICLOCKWISE
+
+EDGES
+0: (0, -1, -1) UF 
+1: (-1, 0, -1) LF
+2: (1, 0, -1) RF
+3: (0, 1, -1) DF
+4: (-1, -1, 0) UL
+5: (1, -1, 0) UR
+6: (-1, 1, 0) DL
+7: (1, 1, 0) DR
+8: (0, -1, 1) UB
+9: (-1, 0, 1) LB
+10: (1, 0, 1) RB
+11: (0, 1, 1) DB
+ORIENT
+0: NORMAL
+1: ABNORMAL
+
+"""
