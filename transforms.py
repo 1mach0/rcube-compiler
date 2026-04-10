4d1af8ea8862c4912fa031cc8e7a439f3825da9a
1mach0 <machiraju.shanmukh@gmail.com>
2026-04-10 18:06:48 +0530

    Fixed orientation bugs

--- transforms.py
+++ transforms.py
@@ -1,4 +1,5 @@
 from models import Cubie
+from utils import neg_vec
 
 def transform_x(pos: Cubie, sign=True):
     x, y, z = pos.coords
@@ -6,33 +7,33 @@ def transform_x(pos: Cubie, sign=True):
 
     if sign:
         pos.coords = (x, -z, y)
-        pos.orient = (ax_x, -ax_z, ax_y)
+        pos.orient = (ax_x, neg_vec(ax_z), ax_y)
     else:
         pos.coords = (x, z, -y)
-        pos.orient = (ax_x, ax_z, -ax_y)
+        pos.orient = (ax_x, ax_z, neg_vec(ax_y))
     
     return pos
 
 def transform_y(pos, sign=True):
-    x, y, z = pos
+    x, y, z = pos.coords
     ax_x, ax_y, ax_z = pos.orient
     if sign:
         pos.coords = (-z, y, x)
-        pos.orient = (-ax_z, ax_y, ax_x)
+        pos.orient = (neg_vec(ax_z), ax_y, ax_x)
     else:
         pos.coords = (z, y, -x)
-        pos.orient = (ax_z, ax_y, -ax_x)
+        pos.orient = (ax_z, ax_y, neg_vec(ax_x))
 
     return pos
 
 def transform_z(pos, sign=True):
-    x, y, z = pos
+    x, y, z = pos.coords
     ax_x, ax_y, ax_z = pos.orient
     if sign:
         pos.coords = (y, -x, z)
-        pos.orient = (ax_y, -ax_x, ax_z)
+        pos.orient = (ax_y, neg_vec(ax_x), ax_z)
     else:
         pos.coords = (-y, x, z)
-        pos.orient = (-ax_y, ax_x, ax_z)
+        pos.orient = (neg_vec(ax_y), ax_x, ax_z)
 
     return pos
--- utils.py
+++ utils.py
@@ -15,3 +15,6 @@ for z in [-1, 0, 1]:
       for x in [-1, 0, 1]:
          if (x + y + z) % 2 == 0 and (x or y or z):
             EDGES.append([x, y, z])
+
+def neg_vec(v):
+   return (-v[0], -v[1], -v[2])
