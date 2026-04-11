451c8e4529fc53fd71fb44886cfd1ddb4f841134
1mach0 <machiraju.shanmukh@gmail.com>
2026-04-11 21:31:42 +0530

    Generated centres via hardcode

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Cubie:
     def __init__(self, pos: tuple, or_x: tuple, or_y: tuple, or_z: tuple):
         self.coords = pos
@@ -28,16 +30,22 @@ for z in [-1, 0, 1]:
             )
             EDGES.append(cubie)
 
-CENTRES = []
-
+CENTRES = [
+    Cubie((0, -1, 0), (1,0,0), (0,1,0), (0,0,1)),  # U
+    Cubie((0,  1, 0), (1,0,0), (0,1,0), (0,0,1)),  # D
+    Cubie((0,  0,-1), (1,0,0), (0,1,0), (0,0,1)),  # F
+    Cubie((0,  0, 1), (1,0,0), (0,1,0), (0,0,1)),  # B
+    Cubie((1,  0, 0), (1,0,0), (0,1,0), (0,0,1)),  # R
+    Cubie((-1, 0, 0), (1,0,0), (0,1,0), (0,0,1)),  # L
+]
 
 def neg_vec(v):
    return (-v[0], -v[1], -v[2])
 
 def orient_to_matrix(orient):
     ax_x, ax_y, ax_z = orient
-    return [
+    return np.array([
         [ax_x[0], ax_y[0], ax_z[0]],
         [ax_x[1], ax_y[1], ax_z[1]],
         [ax_x[2], ax_y[2], ax_z[2]],
-    ]
+    ])
