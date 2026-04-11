d059867e841042247b9ceff12924244cd922c5be
1mach0 <machiraju.shanmukh@gmail.com>
2026-04-11 21:31:24 +0530

    Added body offset and MuJoCo run code

--- render.py
+++ render.py
@@ -5,8 +5,8 @@ import mujoco.viewer
 from models import Cube
 from utils import orient_to_matrix
 
-
 SCALE = 0.04
+BODY_OFFSET = 1
 
 def to_pos(coords):
     return np.array([SCALE * c for c in coords])
@@ -21,15 +21,16 @@ def apply_cube_to_mujoco(cube, data):
     cubies = cube.get_all_cubies()  # you define this
 
     for i, cubie in enumerate(cubies):
-        data.xpos[i] = to_pos(cubie.coords)
-        data.xquat[i] = orient_to_quat(cubie.orient)
+        data.xpos[i + BODY_OFFSET] = to_pos(cubie.coords)
+        data.xquat[i + BODY_OFFSET] = orient_to_quat(cubie.orient)
 
-
-
-model = mujoco.MjModel.from_xml_path("cube.xml")
+model = mujoco.MjModel.from_xml_path("Cube.xml")
 data = mujoco.MjData(model)
 
 rub = Cube()
+
+for i in range(model.nbody):
+    print(i, mujoco.mj_id2name(model, mujoco.mjtObj.mjOBJ_BODY, i))
 
 with mujoco.viewer.launch_passive(model, data) as viewer:
     while viewer.is_running():
@@ -40,5 +41,5 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
 
         # --- push to mujoco ---
         apply_cube_to_mujoco(rub, data)
-
+        mujoco.mj_forward(model, data)
         viewer.sync()
